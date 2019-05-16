5b755adcbbf9eb43e0ca920bd2a4ee5798a8263a
Jacquelyn Collas <46759524+jecollas@users.noreply.github.com>
2019-05-16 14:43:03 -0400

    Add files via upload

--- js/mari-q.js
+++ js/mari-q.js
@@ -28,17 +28,14 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
 
   var captionText = document.getElementById("caption");
-
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  captionText.innerHTML = slides[slideIndex-1].alt;
 }
 
-// Close the Modal
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
