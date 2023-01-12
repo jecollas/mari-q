4940d202bcf793ceebeba7c87af9c946e1a56a6b
Jacquelyn Collas <jackie@moreira.com>
2023-01-12 16:16:45 -0500

    more messing around

--- js/mariq.js
+++ js/mariq.js
@@ -1,5 +1,5 @@
-
 var modal = document.getElementById('artModal');
+
 function openModal() {
   document.getElementById('artModal').style.display = "block";
 }
@@ -8,35 +8,55 @@ function closeModal() {
   document.getElementById('artModal').style.display = "none";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function art(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("myartModal");
-
-  var captionText = document.getElementById("captionDigital");
-  document.getElementById("captionTraditional");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+window.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    modal.style.display = 'none'
   }
-  slides[slideIndex-1].style.display = "block";
-}
+})
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-      }
+// window.onclick = function(event) {
+// if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+document.addEventListener(
+  'click',
+  function(event) {
+    // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+    if (
+      event.target.matches(".close") ||
+      !event.target.closest(".modal")
+    ) {
+      closeModal()
     }
+  },
+  false
+)
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function art(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("myartModal");
+
+//   var captionText = document.getElementById("captionDigital");
+//   document.getElementById("captionTraditional");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex-1].style.display = "block";
+// }
