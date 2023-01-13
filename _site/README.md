159616c2a58c1f1a6b2a519c3b0b1ba5bbdf31c4
Jacquelyn Collas <jackie@moreira.com>
2023-01-12 19:22:03 -0500

    modal

---
+++ _site/README.md
@@ -0,0 +1,2 @@
+# mari-q
+A personal/portfolio website
---
+++ _site/js/mari-q.js
@@ -0,0 +1,43 @@
+// javascript
+
+// Open the Modal
+var modal = document.getElementById('myModal');
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i; //loop command
+  var slides = document.getElementsByClassName("mySlides");
+
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
